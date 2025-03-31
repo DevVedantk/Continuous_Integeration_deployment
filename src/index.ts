80bf120484e4bed21234f4ddf560c3823305b496
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-03-31 11:51:55 +0530

    CI CD init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+app.get("/users",(req,res)=>{
+    res.send("this is backend user page")
+})
+
+app.get("/signIn",(req,res)=>{
+  res.send("signIn page");
+})
+
+app.listen(3000,()=>{
+    console.log("server started");
+})
