9757ade595d5614162d534144ea1652723d7bf40
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-04-03 22:36:46 +0530

    trying... again 4

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,11 +2,8 @@
 function App() {
   
 
-  return <div className="h-screen w-full bg-yellow-400">
+  return <div className="h-screen w-full bg-red-400">
     hello wolrd
-    vedant khasbage
-    ho hee gaya bro
-    pleade ho jana yaar
   </div>
 
 
