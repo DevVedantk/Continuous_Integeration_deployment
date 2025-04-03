27a9332f33039a05535ce252f841754077617f03
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-04-03 22:19:32 +0530

    trying... again 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,40 +7,11 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.get("/testing",(req,res)=>{
-    res.send("testing route today")
+ 
+app.get("/logout",(req,res)=>{
+    res.send("logout")
 })
 
-app.get("/disk",(req,res)=>{
-    res.send("dikshant");
-})
-
-app.get("/final",(req,res)=>{
-    res.send("final testing...")
-})
-
-app.get("/shr",(req,res)=>{
-    res.send("ho gaya reh!!")
-})
-
-app.get("/ved",(req,res)=>{
-    res.send("working vedant bro")
-})
-
-app.get("/users",(req,res)=>{
-    res.send("this is backend user page")
-})
-
-app.get("/test",(req,res)=>{
- res.send("again")
-})
-
-app.get("/test2",(req,res)=>{
-    res.send("again 2")
-   })
-app.get("/signIn",(req,res)=>{
-  res.send("signIn page");
-})
 
 app.listen(3000,()=>{
     console.log("server started");
