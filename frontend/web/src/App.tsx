6d5a9153af23bfc68d601e09d360ef21fd07d76d
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-04-04 08:54:10 +0530

    cicd is working

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,8 +2,10 @@
 function App() {
   
 
-  return <div className="h-screen w-full bg-red-400">
+  return <div className="h-screen w-full bg-blue-800">
    good morning bro!!!
+   bhai itna time lag gaya yaar iise
+   <input type="text" placeholder="hello" />
   </div>
 
 
